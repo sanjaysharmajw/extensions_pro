7f742200280482154f5e4c52236eb979c8e9bcac
Sanjay Sharma <58361466+sanjaysharmajw@users.noreply.github.com>
2024-11-19 15:51:34 +0530

    Package Name correction

--- lib/extentions_pro.dart
+++ lib/extentions_pro.dart
@@ -3,5 +3,5 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
 part 'navigation_extensions_pro.dart';
-part 'widgets_extension_pro.dart';
+part 'widgets_extensions_pro.dart';
 part 'extensions_pro_base.dart';
